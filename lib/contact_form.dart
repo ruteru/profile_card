e5a8776ab97d94789ef94fed37f006934849832f
Andres <garcicon45@gmail.com>
2023-10-01 18:34:42 -0300

    Update
    Contact form added

---
+++ lib/contact_form.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:flutter_google_auth/flutter_google_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}
+final clientId = dotenv.env['GOOGLE_CLIENT_ID'];
+final clientSecret = dotenv.env['GOOGLE_CLIENT_SECRET'];
+
+class ContactFormPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Form'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Future<void> createEvent(DateTime startTime, DateTime endTime, String summary) async {
+  final calendarApi = calendar.CalendarApi(_credentials);
+
+  final event = calendar.Event()
+    ..summary = summary
+    ..start = calendar.EventDateTime.dateTime(startTime)
+    ..end = calendar.EventDateTime.dateTime(endTime);
+
+  await calendarApi.events.insert(event, 'clientId');
+}
+Future<List<DateTime>> fetchAvailableDates() async {
+  final calendarApi = calendar.CalendarApi(_credentials);
+
+  final now = DateTime.now();
+  final endDate = now.add(Duration(days: 30)); // Fetch dates for the next 30 days
+  final events = await calendarApi.events.list('clientId',
+      timeMin: now.toUtc(),
+      timeMax: endDate.toUtc(),
+      singleEvents: true,
+      orderBy: 'startTime');
+
+  final availableDates = events.items.map((event) {
+    final start = event.start.dateTime.toLocal();
+    final end = event.end.dateTime.toLocal();
+    // Check if the event is available (e.g., no existing appointments)
+    if (start.isBefore(end)) {
+      return start;
+    }
+    return null;
+  }).where((date) => date != null).toList();
+
+  return availableDates;
+}
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'contact_form.dart'; 
 
 void main() {
   runApp(MyApp());
@@ -13,70 +14,86 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProfileCard(),
+      home: ProfilePage(),
     );
   }
 }
 
-class ProfileCard extends StatelessWidget {
+class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Profile'),
+        title: Text('Welcome to My Portfolio'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Card(
-          elevation: 5.0,
-          margin: EdgeInsets.all(20.0),
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            width: 300.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 60.0,
-                  backgroundImage: AssetImage('assets/profile.jpg'),
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Andres Garcia',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('assets/andres_garcia.jpeg'),
                   ),
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  'Developer',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.grey,
+                  SizedBox(height: 10.0),
+                  Text(
+                    'Andres Garcia',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.email),
-                    SizedBox(width: 10.0),
-                    Text('garcicon45@gmail.com'),
-                  ],
-                ),
-                SizedBox(height: 10.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.phone),
-                    SizedBox(width: 10.0),
-                    Text('+54 9 11 2469-7598'),
-                  ],
-                ),
-              ],
+                  Text(
+                    'Developer',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+            ListTile(
+              title: Text('About Me'),
+              onTap: () {
+                // Navigate to the About Me page (implement this)
+                Navigator.pop(context); // Close the drawer
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              title: Text('Projects Showcase'),
+              onTap: () {
+                // Navigate to the Projects Showcase page (implement this)
+                Navigator.pop(context); // Close the drawer
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              title: Text('Contact Form'),
+              onTap: () {
+                // Navigate to the Contact Form page
+                Navigator.pop(context); // Close the drawer
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ContactFormPage()));
+              },
+            ),
+          ], // end of children
+        ), // end of child
+      ), // end of drawer
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Your profile content here
+          ],
         ),
       ),
     );
